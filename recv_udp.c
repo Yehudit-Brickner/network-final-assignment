d74c6b28100e92836f084c4cd3305739391e8a80
Yehudit-Brickner <79203360+Yehudit-Brickner@users.noreply.github.com>
2022-07-12 20:06:46 +0300

    packet nit recieved but is being sent

--- recv_udp.c
+++ recv_udp.c
@@ -9,24 +9,25 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/*
+
 void printsin(struct sockaddr_in *s, char *str1, char *str2) {
   printf("%s\n", str1);
   printf("%s: ", str2);
-  -- port: sin->sin_port (host integer type) 
-  -- IP: sin->sin_addr (IP in dotted-decimal notation) 
+  //-- port: sin->sin_port (host integer type) 
+   printf("-- port: %d ", s->sin_port);
+  // -- IP: sin->sin_addr (IP in dotted-decimal notation) 
+  printf("--IP: %s\n", inet_ntoa(s->sin_addr));
   printf("\n");
 }
-*/
 
 int main(int argc, char *argv[]){
-
+  printf("starting the recieve program\n");
   int socket_fd, cc, fsize; // creating  int vars socket_fd, cc, fsize.
   struct sockaddr_in  s_in, from; // creating 2 sockaddr_in structs named s_in, from.
   struct { char head; u_long  body; char tail;} msg; // creatng an unamed struct, the stucts var name is msg it contains 2 chars and an unsigned long.
 
   socket_fd = socket (AF_INET, SOCK_DGRAM, 0); // creating a udp socket. will return socket number, or -1 if there was a problem creating the socket.
-
+ 
   bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */ // changing the memory to '\0' in the struct s_in. before it was all garbage.
   //(starting at the begining f the emory if the struct- its address to the end of the struct -its size)
 
@@ -38,16 +39,20 @@ int main(int argc, char *argv[]){
   s_in.sin_addr.s_addr = htonl(INADDR_ANY);    /* WILDCARD */ 
   s_in.sin_port = htons((u_short)0x3333); 
 
-  //printsin( &s_in, "RECV_UDP", "Local socket is:"); 
+  printsin( &s_in, "RECV_UDP", "Local socket is:"); 
   
   fflush(stdout); // clears the output stream
 
   bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in)); // bindng the socket 
 
   for(;;) {// infinte loop
+    printf("waiting to recieve a packet\n");
     fsize = sizeof(from); // fsize is the size of struct sockaddr_in from
+    printf("fsize= %d\n",fsize);
     cc = recvfrom(socket_fd,&msg,sizeof(msg),0,(struct sockaddr *)&from,&fsize); //recieving the message sent
-    //printsin( &from, "recv_udp: ", "Packet from:"); // printing the message sent the ip and port.
+    printf("cc= %d",cc);
+    printf("recieved packet\n");
+    printsin( &from, "recv_udp: ", "Packet from:"); // printing the message sent, the ip, and the port.
     printf("Got data ::%c%ld%c\n",msg.head,(long) ntohl(msg.body),msg.tail); // priniting out the struct msg.
     fflush(stdout); // clears the output stream
   }
