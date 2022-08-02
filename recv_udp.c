36c243028c0cb74452054c049081c2554146e88b
Yehudit-Brickner <79203360+Yehudit-Brickner@users.noreply.github.com>
2022-08-02 18:00:59 +0300

    fixed comments

--- recv_udp.c
+++ recv_udp.c
@@ -13,10 +13,8 @@
 void printsin(struct sockaddr_in *s, char *str1, char *str2) {
   printf("%s\n", str1);
   printf("%s: ", str2);
-  // -- IP: sin->sin_addr (IP in dotted-decimal notation) 
-  printf("--IP: %s ", inet_ntoa(s->sin_addr));
-  //-- port: sin->sin_port (host integer type) 
-  printf("-- port: %d ", s->sin_port);
+  printf("--IP: %s ", inet_ntoa(s->sin_addr)); // printing the ip number
+  printf("-- port: %d ", s->sin_port); // printing the port
   printf("\n");
 }
 
@@ -24,12 +22,12 @@ int main(int argc, char *argv[]){
  
   int socket_fd, cc, fsize; // creating  int vars socket_fd, cc, fsize.
   struct sockaddr_in  s_in, from; // creating 2 sockaddr_in structs named s_in, from.
-  struct { char head; u_long  body; char tail;} msg; // creatng an unamed struct, the stucts var name is msg it contains 2 chars and an unsigned long.
+  struct { char head; u_long  body; char tail;} msg; // creating an unnamed struct, the stucts var name is msg it contains 2 chars and an unsigned long.
 
   socket_fd = socket (AF_INET, SOCK_DGRAM, 0); // creating a udp socket. will return socket number, or -1 if there was a problem creating the socket.
  
   bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */ // changing the memory to '\0' in the struct s_in. before it was all garbage.
-  //(starting at the begining f the emory if the struct- its address to the end of the struct -its size)
+  //(starting at the begining of the memory of the struct- its address, to the end of the struct -its size)
 
   /* initilizig the 3 parts of the struct s_in: 
   sin_family= address family- ip type
@@ -43,7 +41,7 @@ int main(int argc, char *argv[]){
   printf("\n");
   fflush(stdout); // clears the output stream
 
-  bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in)); // bindng the socket 
+  bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in)); // bindng the socket to the struct s_in 
 
   for(;;) {// infinte loop
     printf("\n");
